16c46da143e68e0c652bb0e4d3863b4306cfa4c4
scarlett park <scarlett@cyalive.com>
2019-05-13 18:37:53 -0400

    add library

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,25 @@
 <template>
-  <h1>CenTeam Fisrt Project</h1>
+  <div>
+    <br>
+    <br>
+    <h1>CenTeam Fisrt Project</h1>
+    <br>
+    <v-avatar size="300">
+      <img
+        style="width:200px, height:200px"
+        src="https://t1.daumcdn.net/cfile/tistory/2467CC3C5806F2380B"
+        alt="handsome"
+      >
+    </v-avatar>
+    <br>
+    <v-avatar size="300">
+      <img
+        style="width:200px, height:200px"
+        src="http://t1.daumcdn.net/liveboard/newsade/6e985d24fe8c4986a63463fd5bca21f6.JPG"
+        alt="handsome"
+      >
+    </v-avatar>
+  </div>
 </template>
 
 <script>
